df7d0db479708ba9a9f7cc334f12c9aa10db66f8
Malachi Danielewicz <mddanielewicz@gmail.com>
2016-06-14 08:18:19 -0400

    wip

--- public/javascripts/social-media.js
+++ public/javascripts/social-media.js
@@ -1,35 +1,42 @@
 //Modal for apt schedule
 
-var aptModal = document.getElementById('apt-modal-wrap');
+var modalWrap = document.getElementById('modal-wrap');
+var modalApt = document.getElementById('modal-apt');
 var aptModalLnk = document.getElementById('nav-apt');
 var aptModalBtn = document.getElementById('schedule-apt');
 
 aptModalLnk.onclick = function(){
-  aptModal.style.display='block';
+  modalWrap.style.display='block';
+  modalApt.style.display='block';
 };  
 
 aptModalBtn.onclick = function(){
-  aptModal.style.display='block';
+  modalWrap.style.display='block';
+  modalApt.style.display='block';
 };  
 
 window.onclick = function(event) {
-    if (event.target == aptModal) {
-        aptModal.style.display = "none";
+    if (event.target == modalWrap) {
+        modalApt.style.display='none';
+        modalWrap.style.display = "none";
     }
 }
 
 //Modal for patient reviews
 
-var reviewModal = document.getElementById('review-modal-wrap');
+var modalWrap = document.getElementById('modal-wrap');
+var modalReview = document.getElementById('review-modal');
 var reviewModalBtn = document.getElementById('review-us');
 
 reviewModalBtn.onclick = function() {
-  reviewModal.style.display = 'block';
+  modalWrap.style.display = 'block';
+  modalReview.style.display = 'block';
   console.log("modal");
 }
 
 window.onclick = function(event) {
-    if (event.target == reviewModal) {
-        reviewModal.style.display = "none";
+    if (event.target == modalWrap) {
+        modalWrap.style.display = 'none';
+        modalReview.style.display = 'none';
     }
 }
