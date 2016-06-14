7372399e4b33f46496e8070e42e080cfba49ac17
Malachi D. Danielewicz <Malachi@cecserver.conrad.com>
2016-06-14 14:00:28 -0400

    Added premium eye screening modal

--- public/javascripts/social-media.js
+++ public/javascripts/social-media.js
@@ -1,7 +1,7 @@
 //Modal for apt schedule
 
 var modalWrap = document.getElementById('modal-wrap');
-var modalApt = document.getElementById('modal-apt');
+var modalApt = document.getElementById('apt-modal');
 var aptModalLnk = document.getElementById('nav-apt');
 var aptModalBtn = document.getElementById('schedule-apt');
 
@@ -15,16 +15,8 @@ aptModalBtn.onclick = function(){
   modalApt.style.display='block';
 };  
 
-window.onclick = function(event) {
-    if (event.target == modalWrap) {
-        modalApt.style.display='none';
-        modalWrap.style.display = "none";
-    }
-}
-
 //Modal for patient reviews
 
-var modalWrap = document.getElementById('modal-wrap');
 var modalReview = document.getElementById('review-modal');
 var reviewModalBtn = document.getElementById('review-us');
 
@@ -34,9 +26,44 @@ reviewModalBtn.onclick = function() {
   console.log("modal");
 }
 
+//Modal Premium Eye Screening
+
+var modalPremium = document.getElementById('premium-modal');
+var premiumModalBtn = document.getElementById('premium-eye')
+
+var glaucomaPkg = document.getElementById('glaucoma-pkg');
+var retinalPkg = document.getElementById('retinal-pkg');
+var glaucomaLnk = document.getElementById('glaucoma-link');
+var retinalLnk = document.getElementById('retinal-link');
+
+premiumModalBtn.onclick = function() {
+    modalWrap.style.display='block';
+    modalPremium.style.display='block';
+    glaucomaLnk.style.backgroundColor='gold';
+}
+
+//Toggle Premium Eye Packages
+
+glaucomaLnk.onclick = function () {
+    glaucomaPkg.style.display='block';
+    glaucomaLnk.style.backgroundColor='gold';
+    retinalPkg.style.display='none';
+    retinalLnk.style.backgroundColor='';
+}
+
+retinalLnk.onclick = function () {
+    glaucomaPkg.style.display='none';
+    glaucomaLnk.style.backgroundColor='rgba(0,0,0,0.0)';
+    retinalPkg.style.display='block';
+    retinalLnk.style.backgroundColor='gold';
+}
+
+// Modal Clear
 window.onclick = function(event) {
     if (event.target == modalWrap) {
-        modalWrap.style.display = 'none';
-        modalReview.style.display = 'none';
+        modalPremium.style.display='none';
+        modalReview.style.display='none';
+        modalApt.style.display="none";
+        modalWrap.style.display='none';
     }
 }
