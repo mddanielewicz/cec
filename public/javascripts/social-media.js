46cebeecb88cdb34c609b765646e3015d6e0cf8c
Malachi Danielewicz <mddanielewicz@gmail.com>
2016-06-14 00:28:27 -0400

    Added review site modal

--- public/javascripts/social-media.js
+++ public/javascripts/social-media.js
@@ -1,19 +1,35 @@
+//Modal for apt schedule
+
 var aptModal = document.getElementById('apt-modal-wrap');
 var aptModalLnk = document.getElementById('nav-apt');
 var aptModalBtn = document.getElementById('schedule-apt');
 
 aptModalLnk.onclick = function(){
   aptModal.style.display='block';
-  console.log("hey");
 };  
 
 aptModalBtn.onclick = function(){
   aptModal.style.display='block';
-  console.log("hey");
 };  
 
 window.onclick = function(event) {
     if (event.target == aptModal) {
         aptModal.style.display = "none";
+    }
+}
+
+//Modal for patient reviews
+
+var reviewModal = document.getElementById('review-modal-wrap');
+var reviewModalBtn = document.getElementById('review-us');
+
+reviewModalBtn.onclick = function() {
+  reviewModal.style.display = 'block';
+  console.log("modal");
+}
+
+window.onclick = function(event) {
+    if (event.target == reviewModal) {
+        reviewModal.style.display = "none";
     }
 }
