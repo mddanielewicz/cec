ca4cc72c51869de21680873127dc0eb167b6d134
Malachi Danielewicz <mddanielewicz@gmail.com>
2016-08-03 21:23:40 -0400

    connected appointment, review, and premium exam form to mongoDB

--- routes/index.js
+++ routes/index.js
@@ -74,7 +74,7 @@ router.get('/contact', function(req, res, next) {
 });
 
 //Post appointment requests
-router.post('/', function(req, res) {
+router.post('/scheduleAppointment', function(req, res) {
   //Set interal DB variable
   var db = req.db;
   
@@ -100,7 +100,8 @@ router.post('/', function(req, res) {
     "scheduleOfficeLocation" : scheduleOfficeLocation,
     "scheduleFirstChoice" : scheduleFirstChoice,
     "scheduleSecondChoice" : scheduleSecondChoice,
-    "scheduleThirdChoice" : scheduleThirdChoice
+    "scheduleThirdChoice" : scheduleThirdChoice,
+    scheduleAptConfirmed : !true //Not working -- will come back to fix.
   }, function(err, doc) {
     if (err) {
       //return error
@@ -114,7 +115,7 @@ router.post('/', function(req, res) {
 });
 
 //Post request for patient review form on homepage
-router.post('/', function(req, res) {
+router.post('/userReview', function(req, res) {
   //Set interal DB variable
   var db = req.db;
   
@@ -151,53 +152,42 @@ router.post('/', function(req, res) {
   });
 });
 
-
-
-
-
-
-
-
-
-//router.get('/userlist', function(req, res){
-//  var db = req.db;
-//  var collection = db.get('usercollection');
-//  collection.find({},{},function(e,docs){
-//    res.render('userlist', {
-//      "userlist" : docs
-//    });
-//  });
-//});
-//
-//router.get('/newuser', function(req, res) {
-//  res.render('newuser', {title: 'Add User'});
-//});
-//
-//router.post('/adduser', function(req, res) {
-//  //Set interal DB variable
-//  var db = req.db;
-//  
-//  //get our form values
-//  var userName = req.body.username;
-//  var userEmail = req.body.useremail;
-//  
-//  //set collection
-//  var collection = db.get('usercollection');
-//  
-//  //submit to db
-//  collection.insert({
-//    "username" : userName,
-//    "useremail" : userEmail
-//  }, function(err, doc) {
-//    if (err) {
-//      //return error
-//      res.send("There was a problem adding your user information to the database.");
-//    }
-//    else{
-//      //forward to seccess page
-//      res.redirect("/");
-//    }
-//  });
-//});
+//Post request for Premium Packagae
+router.post('/premiumPackage', function(req, res) {
+  //Set interal DB variable
+  var db = req.db;
+  
+  //get our form values
+  var premiumFirstName = req.body.premiumFirstName;
+  var premiumLastName = req.body.premiumLastName;
+  var premiumPhone = req.body.premiumPhone;
+  var premiumEmail = req.body.premiumEmail;
+  var premiumPackage = req.body.premiumPackage;
+  var premiumFirstAppointment = req.body.premiumFirstAppointment;
+  var premiumSecondAppointment = req.body.premiumSecondAppointment;
+  
+  //set collection
+  var collection = db.get('premiumPackage');
+  
+  //submit to db
+  collection.insert({
+    "premiumFirstName" : premiumFirstName,
+    "premiumLastName" : premiumLastName,
+    "premiumPhone" : premiumPhone,
+    "premiumEmail" : premiumEmail,
+    "premiumPackage" : premiumPackage,
+    "premiumFirstAppointment" : premiumFirstAppointment,
+    "premiumSecondAppointment" : premiumSecondAppointment
+  }, function(err, doc) {
+    if (err) {
+      //return error
+      res.send("There was a problem scheduling your appointment. Please try again.");
+    }
+    else{
+      //forward to seccess page
+      res.redirect("/");
+    }
+  });
+});
 
 module.exports = router;
